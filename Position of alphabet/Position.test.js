ba8d49109abb4af18054acddf6c3f4e4e8919fc3
Teta Precious <110498537+tpambrose@users.noreply.github.com>
2023-06-13 18:49:06 +0200

    Create Position.test.js

---
+++ Position of alphabet/Position.test.js
@@ -0,0 +1,7 @@
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(position("a"),"Position of alphabet: 1");
+Test.assertEquals(position("z"),"Position of alphabet: 26");
+Test.assertEquals(position("e"),"Position of alphabet: 5");
+  });
+});
